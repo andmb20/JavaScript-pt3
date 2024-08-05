43c67b17bd04320d88b9104a599fdd3105c15bdd
andre <andremorais0000@gmail.com>
2024-08-04 23:51:22 -0300

    deleting gitattributes file. adding Math file with its basic functionalities

---
+++ Math.js
@@ -0,0 +1,28 @@
+/*
+    MATH -> MATEMÁTICA
+    - pow(2,3) potencia  / 2³ = 8 / power
+    - sqrt(25,2) / raiz / √25
+    - PI / 3.141592653589793
+    - ceil / teto -> arredondar para cima
+    - floor / chão -> arredondar para baixo
+
+*/
+ // pow
+const resultpow = Math.pow(2,3)
+console.log(resultpow)
+
+//sqrt
+const resultsqrt = Math.sqrt(25,2)
+console.log(resultsqrt)
+
+//PI
+const resultpi = Math.PI
+console.log(resultpi)
+
+//ceil
+const resultceil = Math.ceil(3.14)
+console.log(resultceil)
+
+//floor
+const resultfloor = Math.floor(3.14)
+console.log(resultfloor)
