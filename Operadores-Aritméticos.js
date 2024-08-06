46e56a3b38a0e6f6a2842968b60accb363346a58
andre <andremorais0000@gmail.com>
2024-08-05 21:42:52 -0300

    Added arithmetic operators

---
+++ Operadores-Aritméticos.js
@@ -0,0 +1,41 @@
+/*
+    Operadores Aritiméticos
+    + -> Adição (Addition)
+    - -> Subtração (Subtraction)
+    * -> Multiplicação (Multiplication)
+    / -> Divisão (Division)
+    % -> Resto (Modulus)
+    ++ -> Incremento (Increment)
+    -- -> Decremento (Decrement)
+    ** -> Exponencial (Exponentiation)
+
+*/
+
+const addition = (2 + 3)
+console.log(addition)
+
+const subtraction = (5 - 2)
+console.log(subtraction)
+
+const multiplication = (3 * 2)
+console.log(multiplication)
+
+const division = (8 / 2)
+console.log(division)
+
+const modulus = (7 / 3)
+console.log(modulus)
+
+let increment = 20
+console.log(increment)
+increment++
+console.log(increment)
+
+let decrement = 15
+console.log(decrement)
+decrement--
+console.log(decrement)
+
+// Similar ao Math.pow
+const exponentiation = 5 ** 7
+console.log(exponentiation)
