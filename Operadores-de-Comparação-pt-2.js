5f5d4d2b51e6dc728439593e52b682168be4d87c
andre <andremorais0000@gmail.com>
2024-08-05 23:44:52 -0300

    Added comparison operators pt2

---
+++ Operadores-de-Comparação-pt-2.js
@@ -0,0 +1,20 @@
+/*
+    Operadores de Comparação (Comparison Operators)
+
+    > = Maior que
+    < = Menor que
+
+    >= -> Maior ou Igual que
+    <= -> Menor ou Igual que
+*/
+
+const firstNumber = 20
+const seccondNumber = 30
+
+console.log(firstNumber > seccondNumber) //Maior que 
+
+console.log(firstNumber < seccondNumber) //Menor que
+
+console.log(firstNumber >= seccondNumber) //Maior ou Igual que
+
+console.log(firstNumber <= seccondNumber) //Menor ou Igual que
