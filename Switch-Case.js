253a7e6b003d7376297180d816ea5ebcde2c13f0
andre <andremorais0000@gmail.com>
2024-08-08 22:30:45 -0300

    Added switch case

---
+++ Switch-Case.js
@@ -0,0 +1,26 @@
+// IF/ELSE SÃO CONTROLADORES DE FLUXO ASSIM COMO TAMBEM O SWITCH CASE
+
+/*
+
+    Switch Case -> Controlador de fluxo
+
+*/
+
+const temperture = 30
+
+switch (temperture) {
+    case 10:
+        console.log("Ta meio Frio")
+        break;
+
+    case 20:
+        console.log("Ta meio Quente")
+        break;
+
+    case 30:
+        console.log("Ta Quente")
+        break;
+
+    default:
+        break;
+}
